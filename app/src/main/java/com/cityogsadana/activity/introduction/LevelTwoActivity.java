eafef19d2c8b8349339956fbb66a5d5be4232edd
Gaurav <gaurav3292@gmail.com>
2017-10-25 17:01:49 +0530

    level 2 inro

---
+++ app/src/main/java/com/cityogsadana/activity/introduction/LevelTwoActivity.java
@@ -0,0 +1,51 @@
+package com.cityogsadana.activity.introduction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.cityogsadana.R;
+import com.cityogsadana.utils.Global;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_level_two)
+public class LevelTwoActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @ViewById(R.id.activity_level_two)
+    ViewGroup viewGroup;
+    @ViewById(R.id.title)
+    TextView title;
+    @ViewById(R.id.back_button)
+    ImageButton backButton;
+
+    @AfterViews
+    public void setData(){
+        Global.setFont(viewGroup, Global.regular);
+        title.setText("Introduction Level 2");
+
+        backButton.setOnClickListener(this);
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back_button:
+                onBackPressed();
+                break;
+        }
+    }
+}
