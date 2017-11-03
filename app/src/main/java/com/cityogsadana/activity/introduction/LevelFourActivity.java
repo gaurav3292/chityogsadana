b0064b9df3cb5c0db014708162b45c7d008b34d2
Gaurav <gaurav3292@gmail.com>
2017-11-03 12:11:20 +0530

    Merge branch 'master' of ssh://github.com/gaurav3292/chityogsadana
    # Conflicts:
    #	app/src/main/res/layout/activity_result.xml

--- app/src/main/java/com/cityogsadana/activity/introduction/LevelFourActivity.java
+++ app/src/main/java/com/cityogsadana/activity/introduction/LevelFourActivity.java
@@ -9,14 +9,25 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.cityogsadana.R;
+import com.cityogsadana.activity.TestActivity_;
+import com.cityogsadana.bean.UserBean;
+import com.cityogsadana.dialogs.ConnectionMessageDialog;
+import com.cityogsadana.interfaces.ActivitySet;
+import com.cityogsadana.utils.AccountChecker;
+import com.cityogsadana.utils.DataEntry;
 import com.cityogsadana.utils.Global;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @EActivity(R.layout.activity_level_four)
-public class LevelFourActivity extends AppCompatActivity implements View.OnClickListener{
+public class LevelFourActivity extends AppCompatActivity implements View.OnClickListener,ActivitySet{
 
     @ViewById(R.id.activity_level_four)
     ViewGroup viewGroup;
@@ -28,12 +39,26 @@ public class LevelFourActivity extends AppCompatActivity implements View.OnClick
     TextView step_one;
     @ViewById(R.id.step_2)
     TextView step_two;
+    @ViewById(R.id.test)
+    TextView testText;
+
+    private UserBean userBean;
+    private ConnectionMessageDialog cDialog = new ConnectionMessageDialog();
+    private DataEntry dataEntry = new DataEntry();
+
 
     @AfterViews
     public  void setData()
     {
         Global.setFont(viewGroup, Global.regular);
-        title.setText("Introduction Level 4");
+        title.setText("Level 4");
+        testText.setVisibility(View.VISIBLE);
+
+        if (userBean.getLevel().getTotalNumberOfDays() != null) {
+            testText.setText("Start Routine");
+        } else {
+            testText.setText("Start Test");
+        }
 
         backButton.setOnClickListener(this);
         step_one.setOnClickListener(this);
@@ -73,6 +98,35 @@ public class LevelFourActivity extends AppCompatActivity implements View.OnClick
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
                 break;
+
+            case R.id.test:
+                if (userBean.getLevel().getTotalNumberOfDays() != null) {
+                    cDialog.showStartRoutine(this, this, "Routine Test", "Do you want to start your routine test?", false);
+                } else {
+                    Date currentTime = Calendar.getInstance().getTime();
+                    DateFormat df = new SimpleDateFormat("HH:mm:ss");
+                    String currentTimeStr = df.format(currentTime);
+                    boolean check = AccountChecker.checkTime(currentTimeStr);
+
+                    if(check)
+                    {
+                        Intent intent1 = new Intent(this, TestActivity_.class);
+                        intent1.putExtra("data",dataEntry.getLevelFourList());
+                        intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent1);
+
+                    }else {
+                        cDialog.successShow(this, "Alert!", "Your test will be active at 21:30:00. (09:30 pm)","Ok", false);
+
+                    }
+
+                }
+                break;
         }
+    }
+
+    @Override
+    public void startRoutineTest() {
+
     }
 }
