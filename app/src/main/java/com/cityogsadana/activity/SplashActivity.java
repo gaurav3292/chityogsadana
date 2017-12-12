c2f24aabc793277e0ed22fe7cfa43114c40e3b76
pc15 <github@karmatech.in>
2017-12-12 15:54:00 +0530

    profile aCtivity

--- app/src/main/java/com/cityogsadana/activity/SplashActivity.java
+++ app/src/main/java/com/cityogsadana/activity/SplashActivity.java
@@ -2,18 +2,16 @@ package com.cityogsadana.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.ViewGroup;
 
 import com.cityogsadana.R;
-import com.cityogsadana.activity.introduction.LevelFourActivity;
-import com.cityogsadana.activity.introduction.LevelFourActivity_;
-import com.cityogsadana.activity.introduction.LevelThreeActivity_;
 import com.cityogsadana.bean.UserBean;
 import com.cityogsadana.prefrences.UserPref;
 import com.cityogsadana.utils.Global;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
@@ -27,12 +25,10 @@ public class SplashActivity extends Activity {
 
     UserBean userBean;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Global.setFont(viewGroup,Global.regular);
-
+    @AfterViews
+    public void setData()
+    {
+        Global.setFont(viewGroup, Global.regular);
         Global.setCustomFont(Global.italic, findViewById(R.id.welcome_txt));
 
         userBean = UserPref.getUser(this);
@@ -58,6 +54,12 @@ public class SplashActivity extends Activity {
             }, timer);
         }
 
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
     }
 }
